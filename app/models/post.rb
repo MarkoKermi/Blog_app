3e85db61a125ef074ec7d1c673031741400ee9d4
Marko Kermiciev <kermicievmarko@gmail.com>
2023-05-04 15:10:22 +0200

    additional changes based on review

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  after_save :update_post_counter
 
   def update_post_counter
     author.update(posts_counter: author.posts.count)
   end
 
+  private
+  
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
