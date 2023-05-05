a82fc3bf5ea5301a60f1096675993ac3f23a28dc
Marko Kermiciev <kermicievmarko@gmail.com>
2023-05-05 17:26:44 +0200

    additional changes based on the review

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -42,4 +42,27 @@ RSpec.describe Post, type: :model do
       expect(subject).to_not be_valid
     end
   end
+  describe 'Functionality' do
+    before { user.update(posts_counter: 0) } # reset posts_counter to 0
+
+    context 'create comments' do
+      it 'returns the 5 most recent comments' do
+        10.times do |i|
+          Comment.create(
+            text: "Comment #{i}",
+            post: subject,
+            author: user
+          )
+        end
+
+        expect(subject.recent_comments.count).to eq(5)
+      end
+    end
+
+    context 'update posts_counter' do
+      it 'updates the posts_counter of the author' do
+        expect { subject.update_post_counter }.to change { user.posts_counter }.from(0).to(1)
+      end
+    end
+  end
 end
