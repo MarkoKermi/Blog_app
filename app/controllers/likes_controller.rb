d1c35bfbc91b0d3c84bf5fc83b5d38105f9c4f51
Marko Kermiciev <kermicievmarko@gmail.com>
2023-05-13 18:43:36 +0200

    deleting commented codes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,5 @@
 class LikesController < ApplicationController
   def create
-    # post = Post.includes(:likes).find(params[:post_id])
-    # likes = post.likes_by(current_user)
-
-    # likes.any? ? likes.each(&:destroy) : Like.create(post:, author:current_user)
-    # # Like.create(post: @post, author: current_user)
-    # # Like.update_likes_counter(@post)
-    # redirect_to user_post_path(@post.author)
-
-    # @post = Post.find(params[:post_id])
-    # Like.create(post: @post, author: current_user)
-    # Like.update_likes_counter(@post)
-    # # redirect_to user_posts_path(@post.author)
-    # post = Post.includes(:likes).find(params[:post_id])
-    # likes = post.likes_by(current_user)
-
-    # likes.any? ? likes.each(&:destroy) : Like.create(post:, author: current_user)
-
-    # redirect_to user_posts_path(post.author)
     user = current_user
     post = Post.find(params[:post_id])
     new_like = Like.create(post: post, author: user)
