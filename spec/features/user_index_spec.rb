8f0167e61cc5c86f387c52ba1a533bfe8745527e
Marko Kermiciev <kermicievmarko@gmail.com>
2023-05-16 09:37:08 +0200

    spec tests for user index

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -38,27 +38,29 @@ require 'capybara'
 
 RSpec.describe 'User index page:', type: :feature do
   before(:each) do
-    @user_one = User.new(name: 'unique', photo: 'http://localhost:3000/anything.jpg', bio: 'Anything test')
-    @user_one.save
-    @post = Post.new(title: 'Anything', text: 'Anything test', author: @user_one)
+    @user1 = User.new(name: 'Marko', photo: 'http://localhost:3000/anything.jpg', bio: 'Anything test')
+    @user1.save
+    @post = Post.new(title: 'Anything', text: 'Anything test', author: @user1)
     @post.save
   end
 
   scenario 'I can see the username of all other users' do
       visit users_path
-      expect(page).to have_content(@user_one.name)
+      expect(page).to have_content(@user1.id)
   end
   scenario 'I can see the profile picture for each user.s' do
     visit users_path
-    expect(@user_one.photo).to match(%r{^http?://.*\.(jpe?g|gif|png)$})
+    expect(@user1.photo).to match(%r{^http?://.*\.(jpe?g|gif|png)$})
   end
   scenario 'I can see the number of posts each user has written.' do
      visit users_path
-     expect(page).to have_content(@user_one.posts.count)
+     expect(page).to have_content(@user1.posts_counter)
   end
  scenario 'When I click on a user, I am redirected to that user show page' do
   visit users_path
-  click_link(@user_one.name)
-  expect(page).to have_current_path(user_path(@user_one.id))
+  find('.user_view', text: @user1.name) do
+  # click_link("#{@user1.name} Number of posts: #{@user1.posts_counter}")
+  expect(page).to have_current_path(user_path(@user1.id))
+  end
  end
 end
